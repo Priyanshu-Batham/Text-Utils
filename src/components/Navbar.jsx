1c585379bdc7c7601ee6ed31c00d53e752ae2a76
Priyanshu Batham <priyanshubatham24733@gmail.com>
2024-05-17 10:31:59 +0000

    fixed href issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = (props) => {
   return (
     <>
-      <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
+      <nav className={`navbar navbar-expand-lg bg-${props.mode}`} id="home">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="#home">
             {props.title}
           </a>
           <button
@@ -22,12 +22,12 @@ const Navbar = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="">
+                <a className="nav-link active" aria-current="page" href="#home">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="#home">
                   About
                 </a>
               </li>
