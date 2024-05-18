f8cf5f25742451dd01f6fd06d52106814de6ddc9
Priyanshu Batham <priyanshubatham24733@gmail.com>
2024-05-18 09:42:28 +0000

    added a favicon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = ({mode}) => {
             Priyanshu Batham
           </figcaption>
         </figure>
-        <button type="button" className="btn btn-info"><Link to="/main" className="nav-link text-dark">App</Link></button>
+        <button type="button" className="btn btn-info"><Link to="/main" className="nav-link text-dark"><strong>App</strong></Link></button>
       </div>
     </>
   );
